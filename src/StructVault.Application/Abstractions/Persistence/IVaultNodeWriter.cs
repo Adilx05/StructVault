7f4bd26d7e3dbc0a388782aad0affed3ef797c6d
Adil <adilx05@gmail.com>
2026-05-09 01:35:10 +0300

    Add vault node delete cascade test

--- src/StructVault.Application/Abstractions/Persistence/IVaultNodeWriter.cs
+++ src/StructVault.Application/Abstractions/Persistence/IVaultNodeWriter.cs
@@ -6,4 +6,6 @@ namespace StructVault.Application.Abstractions.Persistence;
 public interface IVaultNodeWriter
 {
     Task CreateAsync(DbConnection connection, CreateVaultNodeCommand node, CancellationToken cancellationToken);
+
+    Task DeleteAsync(DbConnection connection, DeleteVaultNodeCommand node, CancellationToken cancellationToken);
 }
