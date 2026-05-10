43c2c5d758bfe7f0e22d2466cefb98fea13427c6
Adil <adilx05@gmail.com>
2026-05-10 19:19:27 +0300

    New Version

--- src/StructVault.Desktop/App.xaml.cs
+++ src/StructVault.Desktop/App.xaml.cs
@@ -3,7 +3,7 @@ using System.Windows;
 
 namespace StructVault.Desktop;
 
-public partial class App : Application
+public partial class App : System.Windows.Application
 {
     protected override void OnStartup(StartupEventArgs e)
     {
